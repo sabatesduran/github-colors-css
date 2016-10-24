35c3e149f9d440e247f3653cd72355ccffd36ffb
Dídac Sabatés <sabatesduran@gmail.com>
2016-10-24 09:54:19 +0200

    Ruby script to automatically create the css file and the compiled css file

---
+++ run.rb
@@ -0,0 +1,38 @@
+require 'yaml'
+require 'open-uri'
+
+URL = "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"
+
+# All the names will be changed to be readeable for css
+def to_css_name(e)
+  # Downcase the name
+  css_name = e.downcase
+  # Change the spaces for -
+  css_name = css_name.gsub(/\s/, '-')
+  # Change ' for -
+  css_name = css_name.gsub(/\'/, '-')
+  # Change # for -sharp
+  css_name = css_name.gsub(/\#/, '-sharp')
+  # Change + for p
+  css_name = css_name.gsub(/\+/, 'p')
+end
+
+# Load YAML file
+yalm_hash = YAML.load_file(open(URL))
+
+# Open CSS file
+css_file = File.open("github-colors.css", "w")
+
+yalm_hash.map do |e, i|
+  unless i["color"].nil?
+    # Change programming language name
+    css_name = to_css_name(e);
+    # Adding css background-color class
+    css_file.write(".gh-bgc-#{css_name} { background-color: #{i['color']}; }\n")
+    # Adding css color class
+    css_file.write(".gh-c-#{css_name} { color: #{i['color']}; }\n")
+  end
+end
+
+# Close file
+css_file.close
